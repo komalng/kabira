784a7aa3fcf3cfc4c1b50f93c887c26b316dd11e
Abhishek Gupta <abhishekgupta.iitd@gmail.com>
2020-03-28 15:38:49 +0530

    commit

--- docs/assets/js/video-deck-min.js
+++
@@ -1,1 +0,0 @@
-$(".video-deck .card-body").on("click",function(){$(this).parent().find("a").trigger("click")}),$(".video-deck a").fancybox({caption:function(c,i){return $(this).parent().find(".card-text").html()}});
