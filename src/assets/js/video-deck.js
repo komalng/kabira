3243356a7ffeacc94a6dad834993fc219ac29f53
Abhishek Gupta <abhishekgupta.iitd@gmail.com>
2020-03-29 14:58:57 +0530

    auto play

--- src/assets/js/video-deck.js
+++ src/assets/js/video-deck.js
@@ -1,35 +1,62 @@
-/*
-    Make cards clickable
-    ====================
-*/
+// https://developers.google.com/youtube/iframe_api_reference
 
-$('.video-deck .card-body').on('click', function () {
-    $(this).parent().find("a").trigger('click');
-});
+var player, timer, video_id;
 
-/*
+backToThumbs = () => {
+  $('.card iframe').each((index, value) => {
+    var v_id = value.id;
+    var new_html = `<div style="position: relative;" id="` + v_id + `">
+        <div onclick="playVideoCustom('`+ v_id + `')">
+          <img class="card-img-top img-fluid" src="https://img.youtube.com/vi/`+ v_id + `/mqdefault.jpg">
+          <img src="assets/img/playicon.png" class="playicon" />
+        </div>
+      </div>` + $('#' + v_id).parent().children(":nth-child(2)")[0].outerHTML;
+    $('#' + v_id).parent().html(new_html);
+  });
+}
 
-    Set caption from card text
-    ==========================
-*/
+playVideoCustom = (vid) => {
 
-$('.video-deck a').fancybox({
-    caption: function (instance, item) {
-        return $(this).parent().find('.card-text').html();
+  backToThumbs();
+
+  video_id = vid;
+  player = new YT.Player(video_id, {
+    height: $('#'+vid).height(),
+    width: $('#'+vid).width(),
+    videoId: video_id,
+    playerVars: {
+      autoplay: 1,
+      origin: window.location.origin,
+      autohide: 1,
+      host: `${window.location.protocol}//www.youtube.com`
+    },
+    events: {
+      'onReady': onPlayerReady,
+      'onStateChange': onPlayerStateChange
     }
-});
+  });
+}
 
-// $('a.more').on('click', function(event) {
-//     event.preventDefault();
-//     $.fancybox({
-//         'type' : 'iframe',
-//         // hide the related video suggestions and autoplay the video
-//         'href' : this.href.replace(new RegExp('watch\\?v=', 'i'), 'embed/') + '?rel=0&autoplay=1',
-//         'overlayShow' : true,
-//         'centerOnScroll' : true,
-//         'speedIn' : 100,
-//         'speedOut' : 50,
-//         'width' : 640,
-//         'height' : 480
-//     });
-// });
+// myTimer = (target) => {
+  // target.playVideo();
+// }
+
+function onPlayerReady (event) {
+  event.target.setVolume(100);
+  timer = setInterval(function(){console.log(event.target); event.target.playVideo();}, 2000);
+}
+
+onPlayerStateChange = (event) => {
+  if (event.data == YT.PlayerState.PLAYING) {
+    clearInterval(timer);
+  };
+
+  if (event.data == YT.PlayerState.ENDED) {
+    if ($('#' + video_id).parent().next().html()) {
+      video_id = $('#' + video_id).parent().next().children(":first").attr('id');
+    } else {
+      video_id = $('#Cnm7NQTtFag').parent().parent().children(":first").children(":first").attr('id');
+    }
+    playVideoCustom(video_id)
+  }
+}
